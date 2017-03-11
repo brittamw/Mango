a4e66aca4c2faf9e729331917205fb12a3b9d0a3
Britta <britta-m-walter@t-online.de>
2017-03-11 18:48:45 +0100

    zombie spawnpoints

---
+++ Assets/Scripts/ZombieSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieSpawn : MonoBehaviour {
+
+    public GameObject zombieagent;
+    public GameObject targetObject;
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("SpawnZombie", 5);	
+	}
+	
+    void SpawnZombie()
+    {
+        GameObject zombie = (GameObject)Instantiate(zombieagent, this.transform.position, Quaternion.identity);
+        zombie.GetComponent<AgentNavigationScript>().Target = targetObject.transform;
+        Invoke("SpawnZombie", Random.Range(2, 5));
+    }
+	
+}
