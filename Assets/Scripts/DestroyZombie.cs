5be0534f56f5ab028b81ebd343a3d03fb1037a78
Britta <britta-m-walter@t-online.de>
2017-03-11 20:30:55 +0100

    destroy zombies

---
+++ Assets/Scripts/DestroyZombie.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyZombie : MonoBehaviour {
+    //private float breakForce = 150f;
+
+	// Use this for initialization
+//	void Start () {
+  //      GetComponent<Rigidbody>.collisionDetectionMode = CollosionDetectionMode.Continuous;
+
+            	
+	//}
+	
+	void OnCollisionEnter(Collision collision)
+    {
+       
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (GameObject enemy in enemies)
+            {
+                Destroy(enemy);
+            }
+
+    }
+}
