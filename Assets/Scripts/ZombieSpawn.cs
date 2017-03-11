9002446bde87a0dd9f319cc67f7ebbaa992061d1
Britta <britta-m-walter@t-online.de>
2017-03-11 22:28:18 +0100

    zombie animaton

--- Assets/Scripts/ZombieSpawn.cs
+++ Assets/Scripts/ZombieSpawn.cs
@@ -6,18 +6,22 @@ public class ZombieSpawn : MonoBehaviour {
 
     public GameObject zombieagent;
     public GameObject targetObject;
-
+    int count;
 	// Use this for initialization
 	void Start () {
 
-        Invoke("SpawnZombie", 5);	
+        Invoke("SpawnZombie", 5);
+        
 	}
 	
     void SpawnZombie()
     {
+        if(count < 10) { 
         GameObject zombie = (GameObject)Instantiate(zombieagent, this.transform.position, Quaternion.identity);
         zombie.GetComponent<AgentNavigationScript>().Target = targetObject.transform;
         Invoke("SpawnZombie", Random.Range(2, 5));
+        count++;
+      }
     }
 	
 }
